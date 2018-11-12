889ce08be397809b9488e57da4f8aeefea1feefc
dhondta <alexandre.dhondt@gmail.com>
2018-11-12 15:12:56 +0100

    Added logger to utils and removed CommaListOfInts

--- appmemdumper/utils.py
+++ appmemdumper/utils.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-from argparse import Action
+import logging
 from os import listdir
 from os.path import abspath, basename, isdir, isfile, join, splitext
 from pyunpack import Archive, PatoolError
 
 
-__all__ = ["decompress", "CommaListOfInts"]
+__all__ = ["decompress"]
+logger = logging.getLogger("main")
 
 ARCHIVE_EXCL = lambda f: basename(f) in ["README", "README.md"] or \
                          f.endswith(".txt")
 
 
-def decompress(filename, temp_dir, logger=None):
+def decompress(filename, temp_dir):
     """
     Attempts to decompress the input file to a temporary folder. If Patool fails
      to unpack it, it is assumed that the file is not an archive.
 
     :param filename: path to the input file
     :param temp_dir: temporary directory for decompressed files
-    :param logger:   self-explanatory
     :return:         list of files (these extracted if decompression was
                       performed or the input filename otherwise)
     """
     # set the temporary folder name
-    basename = basename(filename)
-    base, ext = splitext(basename)
+    base, ext = splitext(basename(filename))
     base, ext2 = splitext(base)
     if ext2 != '':
         ext = ext2
@@ -33,8 +32,7 @@ def decompress(filename, temp_dir, logger=None):
     # try to list files from the archive (do not re-decompress if files are
     #  already present)
     if isdir(tmp_dir) and len(listdir(tmp_dir)) > 0:
-        if logger:
-            logger.info("Listing files from '{}'...".format(filename))
+        logger.info("Listing files from '{}'...".format(filename))
         try:
             out = check_output(["patool", "list", filename])
             files, bad = [], False
@@ -52,41 +50,19 @@ def decompress(filename, temp_dir, logger=None):
                 #  the list of file paths
                 return True, files
         except CalledProcessError:
-            if logger:
-                logger.debug("Not an archive, continuing...")
+            logger.debug("Not an archive, continuing...")
             return False, [filename]
     # now extract files
-    if logger:
-        logger.info("Decompressing '{}' (if archive)...".format(filename))
+    logger.info("Decompressing '{}' (if archive)...".format(filename))
     archive = Archive(filename)
     try:
         archive.extractall(tmp_dir, auto_create_dir=True)
     except (PatoolError, ValueError) as e:
         if str(e).startswith("patool can not unpack"):
-            if logger:
-                logger.debug("Not an archive, continuing...")
+            logger.debug("Not an archive, continuing...")
             return False, [filename]
         else:
-            if logger:
-                logger.exception(e)
+            logger.exception(e)
     # retrieve the list of extracted files
     return True, [join(tmp_dir, fn) for fn in listdir(tmp_dir) \
                   if not ARCHIVE_EXCL(fn)]
-
-
-class CommaListOfInts(Action):
-    """ Parses a comma-separated list of ints. """
-    def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        if nargs is not None:
-            raise ValueError("nargs not allowed for CommaListOfInts")
-        super(CommaListOfInts, self).__init__(option_strings, dest, **kwargs)
-
-    def __call__(self, parser, namespace, value, option_string=None):
-        if value == "*":
-            l = list(range(len(DUMPERS)))
-        else:
-            try:
-                l = list(map(int, value.split(',')))
-            except:
-                raise ValueError("{} could not be parsed".format(value))
-        setattr(namespace, self.dest, [DUMPERS[i] for i in l])
