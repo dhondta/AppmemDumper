0b8f2eeccfffa4bc055d8538a2edf1eea2628a4b
Alexandre D'Hondt <alexandre.dhondt@gmail.com>
2019-03-07 16:39:44 +0100

    Minor change (2)

--- reinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pyclean .
-sudo rm -rf build
-sudo rm -rf dist
-sudo rm -rf appmemdumper.egg-info
-sudo pip2 uninstall tinyscript -y
-sudo pip3 uninstall tinyscript -y
-sudo python2 setup.py install
-sudo python3 setup.py install
